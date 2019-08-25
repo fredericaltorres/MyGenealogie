9bc7fe895ef4790afb6d94634b0c4686cc32759c
ftorres <ftorres@emoneyadvisor.com>
2019-08-24 20:03:28 -0400

    Get the data to render in SPA home

--- tools/MyGenealogie.Web/ClientApp/src/components/Home.js
+++ tools/MyGenealogie.Web/ClientApp/src/components/Home.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 
-
 class Home extends Component {
     
     state = {
         persons : []
     };
+
+    componentDidMount() {
+        this.reloadData();
+    }
     
     reloadData = () => {
 
@@ -24,12 +27,25 @@ class Home extends Component {
         this.setState({ ...this.state, [property]: value });
     }
 
+    GetPersonsSelector() {
+        var r = this.state.persons.map((p) => {
+            return (<li key={p.guid}>
+                {p.lastName} {p.maidenName ? ` [${p.maidenName}]` : ``} - {p.firstName} {p.middleName ? ` - ${p.middleName}` : ``}
+            </li>);
+        });
+        return r;
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello, world!</h1>
                 someting
                 <button onClick={this.reloadData}> RELOAD </button>
+                <hr />
+                <ul>
+                    {this.GetPersonsSelector()}
+                </ul>
             </div>
         );
     }
